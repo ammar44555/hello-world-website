2cead35c084a3bb66224f2198453266450d3d6db
ammar44555 <130128991+ammar44555@users.noreply.github.com>
2024-11-13 12:18:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Static files serve karne ke liye
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Home route
 app.get('/', (req, res) => {
-    // Send the index.html file located in the views directory
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Server ko start karna
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
